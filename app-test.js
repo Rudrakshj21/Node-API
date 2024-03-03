ad8f837997211d92f9f79d03c1f3d96b76b150ae
Rudraksh Joshi <rudrakshjoshi27@gmail.com>
2024-03-03 08:53:16 +0530

    express/middleware config/define

---
+++ app-test.js
@@ -0,0 +1,40 @@
+// all middleware/express configuration
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+// 1 MIDDLEWARES
+app.use(morgan('dev'));
+// these more simple middleware functions apply to all request
+app.use(express.json()); //middleware
+// custom middleware
+app.use((req, res, next) => {
+  console.log(
+    'Hello from middleware hehehe '
+  );
+  next(); // NEVER FORGET
+});
+
+app.use((req, res, next) => {
+  req.requestTime =
+    new Date().toISOString();
+
+  next();
+});
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+// 3 ROUTES
+// these routers(middleware functions) only apply to a certain route
+// Mounting new routers on a route
+
+const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./routes/tourRoutes');
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter);
+
+module.exports = app;

766a929236f28b7d5173a731f5b2008f83cb0964
Rudraksh Joshi <rudrakshjoshi27@gmail.com>
2024-03-05 12:41:36 +0530

    connecting to db using mongoose

--- server-test.js
+++ server-test.js
@@ -1,8 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 // console.log(process.env)
 const app = require('./app-test');
-
+(async () => {
+  try {
+    const conStr = process.env.CONNECTION.replace(
+      '<password>',
+      process.env.DB_PASSWORD
+    );
+    await mongoose.connect(conStr);
+    console.log('Connected to DB ✅');
+  } catch (error) {
+    console.log('Cannot connect to DB ❌', error);
+  }
+})();
 // 4 SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
